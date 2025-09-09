c3bd3088437cf7f7e8089330b5bb5f623b07fc2c
olu4P-12 <paulo81206@edu.unipar.br>
2025-09-08 22:32:15 -0300

    Add files via upload

---
+++ VetoresInicializacao/src/Atividade05.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Atividade05 {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        String[] nomes = new String[4];
+
+
+        for (int i = 1; i < nomes.length; i++) {
+            System.out.print("Digite o nome da posição " + i + ": ");
+            nomes[i] = scanner.nextLine();
+        }
+
+
+        System.out.println("\nNomes inseridos:");
+        for (int i = 1; i < nomes.length; i++) {
+            System.out.println("Posição " + i + ": " + nomes[i]);
+        }
+
+
+        scanner.close();
+    }
+}
---
+++ VetoresInicializacao/src/Atividade07.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Atividade07 {
+
+
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+
+            String[] nomes = new String[15];
+
+
+            for (int i = 0; i < nomes.length; i++) {
+                System.out.print("Digite o " + (i + 1) + "º nome: ");
+                nomes[i] = scanner.nextLine();
+            }
+
+
+            System.out.println("\nNomes na ordem inversa:");
+            for (int i = nomes.length - 1; i >= 0; i--) {
+                System.out.println("Posição " + i + ": " + nomes[i]);
+            }
+
+            scanner.close();
+        }
+    }
+
+
